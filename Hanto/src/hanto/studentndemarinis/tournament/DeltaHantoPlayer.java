26fc7f8764a62097ecd539802a38d8051fdf61eb
Nicholas DeMarinis <ndemarinis@wpi.edu>
2013-02-28 08:25:05 -0500

    Revert "Moved FakedStrategy to test.  "
    This reverts commit 90896657f96d834e544327afa9dd7da41c66dd28.

--- Hanto/src/hanto/studentndemarinis/tournament/DeltaHantoPlayer.java
+++ Hanto/src/hanto/studentndemarinis/tournament/DeltaHantoPlayer.java
@@ -27,8 +27,10 @@ import hanto.util.HantoPlayerColor;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.Vector;
 
 /**
